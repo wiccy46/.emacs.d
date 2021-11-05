ba8252fc2930e8d05a197f781b7abe206c207309
wiccy46 <eric1986_chn@yahoo.com>
2021-11-05 21:03:51 +0100

    remove elpa folder

--- elpa/evil-terminal-cursor-changer-20211002.709/evil-terminal-cursor-changer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-terminal-cursor-changer" "20211002.709" "Change cursor shape and color by evil state in terminal" '((evil "1.0.8")) :commit "5b2d76fd26bf33022bbad0198acd9b83c9759750" :authors '(("7696122")) :maintainer '("7696122") :keywords '("evil" "terminal" "cursor") :url "https://github.com/7696122/evil-terminal-cursor-changer")
