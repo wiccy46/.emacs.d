8d38943499dcf84738c0af724c97da1bd98aed1f
jiajun <eric1986_chn@yahoo.com>
2021-11-07 15:11:58 +0100

    Add pyvenv

--- conf/python.el
+++ conf/python.el
@@ -3,3 +3,18 @@
   :hook (python-mode . (lambda ()
 			 (require 'lsp-pyright)
 			 (lsp))))
+
+
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode t)
+
+  ;; Set correct Python interpreter
+  (setq pyvenv-post-activate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter "python3")))))
+
